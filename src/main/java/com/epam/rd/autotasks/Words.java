ce11360d71f0a0c506d6b52ba481608a40d7cf01
Anastasiia Vecherkovska <anastasiia_vecherkovska@epam.com>
2022-11-11 18:58:53 +0200

    Initial commit

---
+++ src/main/java/com/epam/rd/autotasks/Words.java
@@ -0,0 +1,11 @@
+package com.epam.rd.autotasks;
+
+
+import java.util.List;
+
+public class Words {
+
+    public String countWords(List<String> lines) {
+        throw new UnsupportedOperationException();
+    }
+}
